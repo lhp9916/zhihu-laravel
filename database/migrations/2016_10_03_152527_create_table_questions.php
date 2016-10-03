d953a8f343df69ee1dd8d2b6fd119651e6db5f72
lhp9916 <lhp9916@gmail.com>
2016-10-03 23:50:33 +0800

    question migration 建立

---
+++ database/migrations/2016_10_03_152527_create_table_questions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',64);
+            $table->text('desc')->nullable()->comment('问题描述');
+            $table->unsignedInteger('user_id');
+            $table->string('status')->default('ok');
+            $table->timestamps();
+
+            //外键
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
