75247de6566117fc777dfda3a5a70a3e562483d8
lhp9916 <lhp9916@gmail.com>
2016-10-03 15:43:53 +0800

    migration users表建立

--- database/migrations/2016_10_03_071449_create_table_users.php
+++ database/migrations/2016_10_03_071449_create_table_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTableUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->text('avatar_url')->nullable();
+            $table->string('phone')->unique()->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->text('intro')->nullable();
             $table->timestamps();
         });
     }
