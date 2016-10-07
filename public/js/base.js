1387d5249dd5c99971318d5ca33b6e8105530465
lhp9916 <lhp9916@gmail.com>
2016-10-07 23:21:52 +0800

    注册模块-表单验证和美化

--- public/js/base.js
+++ public/js/base.js
@@ -4,28 +4,46 @@
     angular.module('zhihu', [
         'ui.router',
     ])
-        .config(function ($interpolateProvider,      //注入
-                          $stateProvider,
-                          $urlRouterProvider) {
-            $interpolateProvider.startSymbol('[:');
-            $interpolateProvider.endSymbol(':]');
+        .config(['$interpolateProvider',
+            '$stateProvider',
+            '$urlRouterProvider',
+            function ($interpolateProvider,      //注入
+                      $stateProvider,
+                      $urlRouterProvider) {
+                $interpolateProvider.startSymbol('[:');
+                $interpolateProvider.endSymbol(':]');
 
-            $urlRouterProvider.otherwise('/home');
+                $urlRouterProvider.otherwise('/home');
 
-            $stateProvider
-                .state('home', {
-                    url: '/home',
-                    templateUrl: 'home.tpl' // host/home.tpl
-                })
-                .state('login', {
-                    url: '/login',
-                    //template: '<h1>登录</h1>',
-                    templateUrl: 'login.tpl'
-                })
-                .state('signup', {
-                    url: '/signup',
-                    //template: '<h1>登录</h1>',
-                    templateUrl: 'signup.tpl'
-                })
-        })
+                $stateProvider
+                    .state('home', {
+                        url: '/home',
+                        templateUrl: 'home.tpl' // host/home.tpl
+                    })
+                    .state('login', {
+                        url: '/login',
+                        //template: '<h1>登录</h1>',
+                        templateUrl: 'login.tpl'
+                    })
+                    .state('signup', {
+                        url: '/signup',
+                        //template: '<h1>登录</h1>',
+                        templateUrl: 'signup.tpl'
+                    })
+            }])
+        .service('UserService', [function () {
+            var me = this;
+            me.signup_data = {};
+            me.signup = function () {
+
+            }
+        }])
+
+        .controller('SignupController', [
+            '$scope',
+            'UserService',
+            function ($scope, UserService) {
+                $scope.User = UserService;
+            }])
+
 })();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,10 +49,46 @@
     </div>
 </script>
 <script type="text/ng-template" id="signup.tpl">
-    <div class="signup container">
-        <h1>注册</h1>
-        近一个月因为忙于其他事情，一直没能抽出时间来更新项目进度。现在，只能趁着国庆期间，赶紧抽空更新下进度。这次，我想简单谈谈服务端的一些东西。
-        之前，我
+    <div class="signup container" ng-controller="SignupController">
+        <div class="card">
+            <h1>注册</h1>
+            [: User.signup_data :]
+            <form name="signup_form" ng-submit="User.signup()">
+                <div class="input-group">
+                    <label>用户名:</label>
+                    <input name="username" type="text"
+                           ng-minlength="4"
+                           maxlength="16"
+                           required
+                           ng-model="User.signup_data.username"/>
+
+                    <div class="input-error-set" ng-if="signup_form.username.$touched">
+                        <div ng-if="signup_form.username.$error.required">用户名为必填项</div>
+                        <div ng-if="signup_form.username.$error.minlength || signup_form.username.$error.maxlength">
+                            用户名长度需在4至16位之间
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <label>密码:</label>
+                    <input name="password" type="password"
+                           ng-minlength="6"
+                           required
+                           ng-model="User.signup_data.password"/>
+
+                    <div class="input-error-set" ng-if="signup_form.password.$touched">
+                        <div ng-if="signup_form.password.$error.required">密码为必填项</div>
+                        <div ng-if="signup_form.password.$error.minlength">
+                            密码至少为6位
+                        </div>
+                    </div>
+                </div>
+                <button type="submit"
+                        ng-disabled="signup_form.$invalid">
+                    注册
+                </button>
+            </form>
+        </div>
     </div>
 </script>
 </html>
